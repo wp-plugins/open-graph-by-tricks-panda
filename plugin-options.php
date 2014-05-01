4193d657e5983aed7ddea3816cdb7c9db38cbaea
hardeepasrani <hardeepasrani@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-01 13:36:31 +0000

    Added 1.0 revision.
    git-svn-id: https://plugins.svn.wordpress.org/open-graph-by-tricks-panda/tags/1.0@906293 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin-options.php
@@ -0,0 +1,69 @@
+<?php
+
+function tricks_panda_og_register_settings()
+{
+	add_option('tricks_panda_og_default');
+	add_option('tricks_panda_og_site_type');
+	add_option('tricks_panda_og_fb_page');
+	add_option('tricks_panda_og_app_id');
+	register_setting('default', 'tricks_panda_og_default');
+	register_setting('default', 'tricks_panda_og_site_type');
+	register_setting('default', 'tricks_panda_og_fb_page');
+	register_setting('default', 'tricks_panda_og_app_id');
+}
+
+add_action('admin_init', 'tricks_panda_og_register_settings');
+
+function tricks_panda_og_register_options_page()
+{
+	add_options_page('Open Graph', 'Open Graph', 'manage_options', 'open-graph-options', 'open_graph_options_page');
+}
+
+add_action('admin_menu', 'tricks_panda_og_register_options_page');
+
+function open_graph_options_page()
+{
+?>
+<div class="wrap">
+	<?php
+	screen_icon(); ?>
+	<h2>Open Graph By Tricks Panda</h2>
+	<form method="post" action="options.php"> 
+		<?php
+	settings_fields('default'); ?>
+		<h3>You can configure Open Graph settings from the form below:</h3>
+			<table class="form-table">
+				<tr valign="top">
+					<th scope="row" style="width:350px;"><label for="tricks_panda_og_default">Website Logo (Default Image):</label></th>
+					<td><input type="text" size="50px" id="tricks_panda_og_default" name="tricks_panda_og_default" value="<?php
+	echo get_option('tricks_panda_og_default'); ?>"/></td>
+				</tr>
+				<tr valign="top">
+					<th scope="row"><label for="tricks_panda_og_site_type">Site Type:</label></th>
+					<td><select id="tricks_panda_og_site_type" name="tricks_panda_og_site_type" value=" <?php
+	echo get_option('tricks_panda_og_site_type'); ?>">
+					    <option value="website" <?php
+	if (get_option('tricks_panda_og_site_type') == website) echo 'selected="selected"'; ?>>Website</option>
+					    <option value="blog" <?php
+	if (get_option('tricks_panda_og_site_type') == blog) echo 'selected="selected"'; ?>>Blog</option>
+					  </select></td>
+				</tr>
+				<tr valign="top">
+					<th scope="row" style="width:350px;"><label for="tricks_panda_og_fb_page">Facebook Page URL:</label></th>
+					<td><input type="text" size="50px" id="tricks_panda_og_fb_page" name="tricks_panda_og_fb_page" value="<?php
+	echo get_option('tricks_panda_og_fb_page'); ?>" /></td>
+				</tr>
+				<tr valign="top">
+					<th scope="row" style="width:350px;"><label for="tricks_panda_og_app_id">Facebook APP ID:</label></th>
+					<td><input type="text" size="50px" id="tricks_panda_og_app_id" name="tricks_panda_og_app_id" value="<?php
+	echo get_option('tricks_panda_og_app_id'); ?>" /></td>
+				</tr>
+			</table>
+		<?php
+	submit_button(); ?>
+	</form>
+</div>
+<?php
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,64 @@
+=== Open Graph By Tricks Panda ===
+Plugin URI: http://www.trickspanda.com
+Description: Easily add Open Graph tags to your WordPress website to make it ready to share on Facebook.
+Version: 1.0
+Requires at least: 2.5
+Tested up to: 3.9
+Contributors: hardeepasrani
+Author URI: http://www.hardeepasrani.com
+Tags: mata tags, open graph, opengraph, facebook, social, header
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+There are dozens of plugins on the WordPress repository with hundreds of options and advertisements. So, we created an easy to use Open Graph plugin for your WordPress. We don't offer a premium or paid version, but all we offer is this completely ad-free version for free.
+
+Our plugin adds following tags to your WordPress, according to post/page type:
+
+`<meta property="og:locale" content="en_US" />`
+
+`<meta property="og:site_name" content="Website Name" />`
+
+`<meta property="og:title" content="Page Title" />`
+
+`<meta property="og:url" content="http://www.trickspanda.com" />`
+
+`<meta property="og:type" content="website" />`
+
+`<meta property="og:description" content="Description of your website goes here." />`
+
+`<meta property="og:image" content="image-url.png" />`
+
+`<meta property="article:author" content="Facebook URL of Post Author" />`
+
+`<meta property="article:published_time" content="Time of Post Published" />`
+
+`<meta property="article:modified_time" content="Time of Post last modified" />`
+
+`<meta property="og:updated_time" content="Time of Post last modified" />`
+
+`<meta property="article:publisher" content="Facebook Page of Website." />`
+
+`<meta property="fb:app_id" content="Facebook APP ID" />`
+
+And that's now all. We will add more features in the coming versions of the plugin.
+
+== Installation ==
+
+1. Upload the plugin to your 'wp-content/plugins' directory, or download and install automatically through your admin panel.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Configure Open-Graph settings from the 'Settings > Open-Graph' page.
+4. Don't forget to add author's Facebook profile from 'Users > Your Profile' page.
+
+== Frequently Asked Questions ==
+
+= How to donate or contribute? =
+
+I'm a teen WordPresser, so I don't have a Paypal account, so just simply support this plugin by visiting our website <a href="http://www.trickspanda.com">Tricks Panda</a>.
+
+== Screenshots ==
+
+1. Open-Graph Settings Panel.
+
+2. Add Facebook Author URL From 'Users > Your Profile' Page.
